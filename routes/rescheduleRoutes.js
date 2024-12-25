8f3c97d192abdb2c012c4846499d8b8af3e5db62
ruchilamelinda <ruchilamelinda@gmail.com>
2024-12-25 21:32:09 +0700

    pesan

--- routes/rescheduleRoutes.js
+++ routes/rescheduleRoutes.js
@@ -5,7 +5,6 @@ const rescheduleController = require('../controllers/rescheduleController');
 // Endpoint untuk mendapatkan detail penyewaan dan properti
 router.get('/order/:idPenyewaan/:idProperti', rescheduleController.getOrderDetails);
 
-// Endpoint untuk memperbarui tanggal mulai
 router.put('/order/:idPenyewaan', rescheduleController.updateTanggalMulai);
 
 module.exports = router;
